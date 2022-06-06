e0223b2bbc2084ca575cefb878de7348e081754a
Gautamjha31 <b190051@nitsikkim.ac.in>
2022-06-05 23:34:54 -0200

    initial commit

---
+++ diamond_using_star_pattern.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    cout<<"Enter the number of pattern :- ";
+    cin>>n;
+
+    int space=(2*n-1)/2;
+    for (int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=space;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++)
+        {
+            cout<<"*";
+        }
+     cout<<endl;
+    space--;
+        }
+
+    space=0;
+     for (int i=n; i>=1; i--)
+    {
+        for(int j=1; j<=space;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++)
+        {
+          cout<<"*";
+        }
+
+    cout<<endl;
+    space++;
+
+    }
+
+    return 0;
+
+}
